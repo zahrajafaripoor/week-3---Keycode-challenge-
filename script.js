571d207775b9515fc5883a3ff947d7ab0798d2ec
Zahra Jafaripoor <zahrajafaripoor1394@gmail.com>
2024-08-16 10:02:51 +0100

    final check

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.addEventListener('keydown', (event) => {
-    // Update the key code in the large box
+    //  the key code in the large box
     document.getElementById('key-code').textContent = event.which;
   
-    // Update the information in the boxes
+    //  the information in the boxes
     document.getElementById('code').textContent = event.code;
     document.getElementById('key').textContent = event.key;
     document.getElementById('which').textContent = event.which;
